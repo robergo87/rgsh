26e5ec51744ea8d13b6c8427272d12c403919ddc
Robert Górzyński <robert.goorzynski@gmail.com>
2020-08-11 15:01:22 +0200

    mouse click bindings

--- treemenu.sh
+++ treemenu.sh
@@ -27,7 +27,7 @@ fi
 if [ "$1" = "display" ]; then
 	$DIR/filetree.py init "$pid" "$(pwd)"
 	$DIR/filetree.py "print" "$pid" | $DIR/fzf/bin/fzf --layout reverse-list --delimiter ";" --with-nth -1 --ansi \
-		--bind "home:execute-silent($DIR/treemenu.sh home {} $pid $2)+reload($DIR/filetree.py 'print' $pid),enter:execute-silent($DIR/treemenu.sh select {} $pid $2)+reload($DIR/filetree.py 'print' $pid),space:execute($DIR/fm.sh menu {} < /dev/tty)+reload($DIR/filetree.py 'print' $pid)"
+		--bind "home:execute-silent($DIR/treemenu.sh home {} $pid $2)+reload($DIR/filetree.py 'print' $pid),left-click:execute-silent($DIR/treemenu.sh select {} $pid $2)+reload($DIR/filetree.py 'print' $pid),double-click:execute-silent($DIR/treemenu.sh select {} $pid $2)+reload($DIR/filetree.py 'print' $pid),enter:execute-silent($DIR/treemenu.sh select {} $pid $2)+reload($DIR/filetree.py 'print' $pid),space:execute($DIR/fm.sh menu {} < /dev/tty)+reload($DIR/filetree.py 'print' $pid)"
 	$DIR/filetree.py destroy "$pid"
 	exit 0
 fi
